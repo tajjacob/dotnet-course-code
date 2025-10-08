f919ba2bcba1d3e6f7e08190556f458c7a674eef
tajjacob <63710008+tajjacob@users.noreply.github.com>
2025-10-08 10:18:41 +0800

    🎉 project: start new project

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!"); // new line added
+Console.Write("Press any key to exit..."); // write on the same line
