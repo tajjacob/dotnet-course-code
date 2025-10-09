905fa6760d724facd4dd4681714103fec6bd5d34
tajjacob <63710008+tajjacob@users.noreply.github.com>
2025-10-09 10:23:07 +0800

    ⚗️ tutorial: 16. Console App Template
    - add .gitignore file

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!"); // new line added
-Console.Write("Press any key to exit..."); // write on the same line
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
