38d8e3035f1fc116e760e368366735c2bee2a9ae
tajjacob <63710008+tajjacob@users.noreply.github.com>
2025-12-01 15:37:18 +0800

    ⚗️ tutorial: 49. Model Mapping with AutoMapper ✨ feat: Implement snake_case to PascalCase JSON mapping
    Introduces two methods for deserializing JSON with snake_case property names into C# objects that use the PascalCase naming convention.
    
    - **AutoMapper Implementation**:
      - Adds `ComputerSnake` model to represent the incoming JSON structure.
      - Configures AutoMapper to map from `ComputerSnake` to the `Computer` model.
      - This demonstrates a robust way to handle object-to-object mapping.
    
    - **System.Text.Json with `[JsonPropertyName]`**:
      - Utilizes the `[JsonPropertyName]` attribute on the `Computer` model properties.
      - This provides a direct and efficient way to deserialize the JSON without an intermediate model or an external library.
    
    Both approaches are demonstrated in `Program.cs` by reading `ComputersSnake.json` and verifying the deserialization.

--- Models/Computer.cs
+++ Models/Computer.cs
@@ -1,15 +1,25 @@
+using System.Text.Json.Serialization;
+
 namespace HelloWorld.Models
 {
     public class Computer
     {
         // private string _motherboard;
+        [JsonPropertyName("computer_id")]
         public int ComputerId { get; set; }
+        [JsonPropertyName("motherboard")]
         public string Motherboard { get; set; } = "";
+        [JsonPropertyName("cpu_cores")]
         public int? CPUCore { get; set; }
+         [JsonPropertyName("has_wifi")]
         public bool HasWifi { get; set; }
+        [JsonPropertyName("has_lte")]
         public bool HasLTE { get; set; }
+        [JsonPropertyName("release_date")]
         public DateTime? ReleaseDate { get; set; }
+        [JsonPropertyName("price")]
         public decimal Price { get; set; }
+        [JsonPropertyName("video_card")]
         public string VideoCard { get; set; } = "";
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// 47. JSON - Reading from and Writing to JSON Files in C#
+﻿// 49. Model Mapping with AutoMapper 
 /// Commands to run in terminal:
 /// cd /Users/tajjacob/Documents/GitHub/dotnet-course-code/HelloWorld
 /// dotnet --info
@@ -11,6 +11,7 @@ using System.Data;
 using System.Globalization;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using AutoMapper;
 using Dapper;
 using HelloWorld.Data;
 using HelloWorld.Models;
@@ -66,66 +67,109 @@ namespace HelloWorld
 
             // openFile.Close();
 
-            string computersJson = File.ReadAllText("Computers.json");
+            // string computersJson = File.ReadAllText("Computers.json");
+            string computersJson = File.ReadAllText("ComputersSnake.json");
+
+             Mapper mapper = new Mapper(new MapperConfiguration((cfg) => {
+                cfg.CreateMap<ComputerSnake, Computer>()
+                    .ForMember(destination => destination.ComputerId, options => 
+                        options.MapFrom(source => source.computer_id))
+                    .ForMember(destination => destination.CPUCore, options => 
+                        options.MapFrom(source => source.cpu_cores))
+                    .ForMember(destination => destination.HasLTE, options => 
+                        options.MapFrom(source => source.has_lte))
+                    .ForMember(destination => destination.HasWifi, options => 
+                        options.MapFrom(source => source.has_wifi))
+                    .ForMember(destination => destination.Motherboard, options => 
+                        options.MapFrom(source => source.motherboard))
+                    .ForMember(destination => destination.VideoCard, options => 
+                        options.MapFrom(source => source.video_card))
+                    .ForMember(destination => destination.ReleaseDate, options => 
+                        options.MapFrom(source => source.release_date))
+                    .ForMember(destination => destination.Price, options => 
+                        options.MapFrom(source => source.price));
+            }));
+
+                        IEnumerable<ComputerSnake>? computersSystem = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<ComputerSnake>>(computersJson); // explanation: using System.Text.Json package with Automapper for mapping snake_case to PascalCase
+
+                          if (computersSystem != null)
+            {
+                IEnumerable<Computer> computerResult = mapper.Map<IEnumerable<Computer>>(computersSystem);
+                Console.WriteLine("Automapper Count: " +  computerResult.Count());
+                // foreach (Computer computer in computerResult)
+                // {
+                //     Console.WriteLine(computer.Motherboard);
+                // }
+            }
+
+            IEnumerable<Computer>? computersJsonPropertyMapping = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson);
+            if (computersJsonPropertyMapping != null)
+            {
+                Console.WriteLine("JSON Property Count: " + computersJsonPropertyMapping.Count());
+                // foreach (Computer computer in computersJsonPropertyMapping)
+                // {
+                //     Console.WriteLine(computer.Motherboard);
+                // }
+            }
 
             // Console.WriteLine(computersJson);
 
-            JsonSerializerOptions options = new JsonSerializerOptions // explanation: options to match camelCase naming in the JSON file
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase // explanation: matches the camelCase naming in the JSON file
-            };
+            // JsonSerializerOptions options = new JsonSerializerOptions // explanation: options to match camelCase naming in the JSON file
+            // {
+            //     PropertyNamingPolicy = JsonNamingPolicy.CamelCase // explanation: matches the camelCase naming in the JSON file
+            // };
 
-            IEnumerable<Computer>? computersNewtonSoft = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computersJson); // explanation: using Newtonsoft.Json package is easier for this case
+            // IEnumerable<Computer>? computersNewtonSoft = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computersJson); // explanation: using Newtonsoft.Json package is easier for this case
 
-            IEnumerable<Computer>? computersSystem = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson, options);  // explanation: System.Text.Json requires more setup for this case
-
-
-            if (computersNewtonSoft != null)
-            {
-                foreach (Computer computer in computersNewtonSoft)
-                {
-                    string sql = @"INSERT INTO TutorialAppSchema.Computer (
-                        Motherboard,
-                        CPUCore,
-                        HasWifi,
-                        HasLTE,
-                        ReleaseDate,
-                        Price,
-                        VideoCard
-                    ) VALUES (@Motherboard,
-                        @CPUCore,
-                        @HasWifi,
-                        @HasLTE,
-                        @ReleaseDate,
-                        @Price,
-                        @VideoCard)";
-
-                    dapper.ExecuteSqlWithRowCount(sql, computer);
-                }
-            }
-
-            JsonSerializerSettings settings = new JsonSerializerSettings // explanation: settings to match camelCase naming in the JSON file
-            {
-                ContractResolver = new CamelCasePropertyNamesContractResolver()
-            };
+            // IEnumerable<Computer>? computersSystem = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson, options);  // explanation: System.Text.Json requires more setup for this case
 
 
-            string computersCopyNewtonsoft = JsonConvert.SerializeObject(computersNewtonSoft, settings);
+            // if (computersNewtonSoft != null)
+            // {
+            //     foreach (Computer computer in computersNewtonSoft)
+            //     {
+            //         string sql = @"INSERT INTO TutorialAppSchema.Computer (
+            //             Motherboard,
+            //             CPUCore,
+            //             HasWifi,
+            //             HasLTE,
+            //             ReleaseDate,
+            //             Price,
+            //             VideoCard
+            //         ) VALUES (@Motherboard,
+            //             @CPUCore,
+            //             @HasWifi,
+            //             @HasLTE,
+            //             @ReleaseDate,
+            //             @Price,
+            //             @VideoCard)";
 
-            File.WriteAllText("computersCopyNewtonsoft.txt", computersCopyNewtonsoft);
+            //         dapper.ExecuteSqlWithRowCount(sql, computer);
+            //     }
+            // }
 
-            string computersCopySystem = System.Text.Json.JsonSerializer.Serialize(computersSystem, options);
+            // JsonSerializerSettings settings = new JsonSerializerSettings // explanation: settings to match camelCase naming in the JSON file
+            // {
+            //     ContractResolver = new CamelCasePropertyNamesContractResolver()
+            // };
 
-            File.WriteAllText("computersCopySystem.txt", computersCopySystem);
+
+            // string computersCopyNewtonsoft = JsonConvert.SerializeObject(computersNewtonSoft, settings);
+
+            // File.WriteAllText("computersCopyNewtonsoft.txt", computersCopyNewtonsoft);
+
+            // string computersCopySystem = System.Text.Json.JsonSerializer.Serialize(computersSystem, options);
+
+            // File.WriteAllText("computersCopySystem.txt", computersCopySystem);
 
 
         }
         
-        static string escapeSingleQuote(string input)
-        {
-            string output = input.Replace("'", "''");
-            return output;
-        }
+        // static string escapeSingleQuote(string input)
+        // {
+        //     string output = input.Replace("'", "''");
+        //     return output;
+        // }
 
     }
 }
