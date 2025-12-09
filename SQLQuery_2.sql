379e64157385dc44762a6016f8a50cbcbda43d6d
tajjacob <63710008+tajjacob@users.noreply.github.com>
2025-12-09 12:27:56 +0800

    ⚗️ tutorial: 56. SQL intermediate
    - learn: CROSS APPLY VS. OUTER APPLY

--- SQLQuery_2.sql
+++ SQLQuery_2.sql
@@ -104,7 +104,40 @@ JOIN TutorialAppSchema.UserSalary AS UserSalary -- join meaning: only those reco
 WHERE Users.Active = 1 
 GROUP BY [UserJobInfo].[Department]
 ORDER BY ISNULL([UserJobInfo].[Department], 'No Department Listed')  DESC -- ordering by Department in descending order, if want to order by Salary use ORDER BY Salary DESC
-         
+
+
+SELECT [Users].[UserId],
+[Users].[FirstName] + ' ' + [Users].[LastName] AS FullName,
+[UserJobInfo].[JobTitle],
+[UserJobInfo].[Department],
+DepartmentAverage.AvgSalary,
+[UserSalary].[UserId],
+[UserSalary].[Salary],
+[Users].[Email],
+[Users].[Gender],
+[Users].[Active]
+FROM TutorialAppSchema.Users AS Users
+-- INNER JOIN
+JOIN TutorialAppSchema.UserSalary AS UserSalary -- join meaning: only those records that have matching UserId in both tables
+    ON UserSalary.UserId = Users.UserId 
+    LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo -- left join meaning: all records from left table (Users) and matching records from right table (UserJobInfo)
+        ON UserJobInfo.UserId = Users.UserId 
+    -- OUTER APPLY ( -- explanation: OUTER APPLY works like a LEFT JOIN but allows to join with a table-valued function or subquery that can reference columns from the left table
+                  CROSS APPLY ( -- explanation: CROSS APPLY works like an INNER JOIN but allows to join with a table-valued function or subquery that can reference columns from the left table
+                  SELECT ISNULL([UserJobInfo2].[Department], 'No Department Listed') AS Department,
+            AVG([UserSalary2].[Salary]) AS AvgSalary
+            FROM TutorialAppSchema.UserSalary AS UserSalary2 -- join meaning: only those records that have matching UserId in both tables
+              
+                LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo2 -- left join meaning: all records from left table (Users) and matching records from right table (UserJobInfo)
+                    ON UserJobInfo2.UserId = UserSalary2.UserId 
+            -- WHERE ISNULL([UserJobInfo2].[Department], 'No Department Listed') = ISNULL([UserJobInfo].[Department], 'No Department Listed') -- explanation: correlating subquery to get average salary for the same department as the outer query
+            WHERE [UserJobInfo2].[Department] = [UserJobInfo].[Department]
+            GROUP BY [UserJobInfo2].[Department]
+    ) AS DepartmentAverage   
+WHERE Users.Active = 1 
+ORDER BY Users.UserId DESC
+
+
 -- SELECT  [UserId]
 --         , [FirstName]
 --         , [LastName]
